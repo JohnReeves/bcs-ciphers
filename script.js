9949335d80d3324bfa3cefada6438f2fb003928e
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2019-11-23 09:25:31 +0000

    changed hello world to a longer string

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
-var yourname = "hello josh";
+var yourname = "now is the winter of our discontent made glorious by this son of York";
 var shift = 5;
 
 var caesarSecret = $("#caesarSecret");
 var isEncodedInCaesar = false;
 
-caesarSecret.text(yourname);
 
-caesarSecret.click(function() {
+
+caesarSecret.click(function () {
+
+  caesarSecret.text(yourname);
 
   if (isEncodedInCaesar == false) {
     yourname = encodeCaesar(yourname, shift)
@@ -19,33 +21,33 @@ caesarSecret.click(function() {
   caesarSecret.text(yourname);
 });
 
-function encodeCaesar(str,shift){
-    /**
-     *  Encode the Caesar Cipher by shifting
-     *   each character by a fixed amount 
-    */
-    var ret = '';
-    var i   = 0;
+function encodeCaesar(str, shift) {
+  /**
+   *  Encode the Caesar Cipher by shifting
+   *   each character by a fixed amount 
+  */
+  var ret = '';
+  var i = 0;
 
-    while (i < str.length) {
-        ret += String.fromCharCode(str.charCodeAt(i) + shift);
-        i   += 1;
-    }
-    return ret;
+  while (i < str.length) {
+    ret += String.fromCharCode(str.charCodeAt(i) + shift);
+    i += 1;
+  }
+  return ret;
 }
 
-function decodeCaesar(str,shift){
-    /**
-     *  Decode the Caesar Cipher by shifting
-     *  by shifting each character back again
-     *  by the same amount
-    */
-    var ret = '';
-    var i   = 0;
+function decodeCaesar(str, shift) {
+  /**
+   *  Decode the Caesar Cipher by shifting
+   *  by shifting each character back again
+   *  by the same amount
+  */
+  var ret = '';
+  var i = 0;
 
-    while (i < str.length) {
-        ret += String.fromCharCode(str.charCodeAt(i) - shift);
-        i   += 1;
-    }
-    return ret;
+  while (i < str.length) {
+    ret += String.fromCharCode(str.charCodeAt(i) - shift);
+    i += 1;
+  }
+  return ret;
 }
